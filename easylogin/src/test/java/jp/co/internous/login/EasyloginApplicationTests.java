c7352e3a943b947156b61b449fdbd178884a9014
Your Name <banirasukai0705@gmail.com>
2023-10-25 19:27:50 +0900

    easylogin

---
+++ easylogin/src/test/java/jp/co/internous/login/EasyloginApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.internous.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
